33d0aff9a87b4a3196f53b425b68306d2cd9ccea
ItsFuntum <elias.morad2008@gmail.com>
2025-11-05 10:37:47 +0100

    basically just a port of the 3ds client at this point

--- source/main.cpp
+++ source/main.cpp
@@ -11,6 +11,9 @@
 #include <map>
 #include <string>
 #include <romfs-wiiu.h>
+#include <deque>
+#include <fcntl.h>
+#include <errno.h>
 
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL.h>
@@ -19,63 +22,43 @@
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8961
 
-void send_chat_line(int *sock, int *in_len, char *input) {
-    if (*in_len > 0) {
-        if (*sock >= 0) {
-            char sendbuf[600];
-            if (send(*sock, sendbuf, strlen(sendbuf), 0) >= 0) {
-                *in_len = 0;
-                input[0] = '\0';
-            }
-        } else {
-            WHBLogPrintf("Send failed, trying to reconnect...");
+// -----------------------
+// Chat buffer
+// -----------------------
+std::deque<std::string> g_ChatBuffer;
+const size_t MAX_CHAT_LINES = 10;
 
-            // Close old socket
-            if (*sock >= 0) close(*sock);
-            *sock = -1;
-
-            // Reconnect
-            *sock = socket(AF_INET, SOCK_STREAM, 0);
-            if (*sock < 0) {
-                WHBLogPrintf("Failed to create socket");
-                return;
-            }
-
-            struct sockaddr_in serverAddr;
-            memset(&serverAddr, 0, sizeof(serverAddr));
-            serverAddr.sin_family = AF_INET;
-            serverAddr.sin_port = htons(SERVER_PORT);
-            serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-            if (connect(*sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == 0) {
-                WHBLogPrintf("Reconnected successfully!");
-            } else {
-                WHBLogPrintf("Failed to reconnect, try again.");
-                close(*sock);
-                *sock = -1;
-            }
-        }
-    }
+void AddChatLine(const std::string &msg)
+{
+    if (g_ChatBuffer.size() >= MAX_CHAT_LINES)
+        g_ChatBuffer.pop_front();
+    g_ChatBuffer.push_back(msg);
 }
 
-// Store font
+// -----------------------
+// Font system
+// -----------------------
 std::map<int, TTF_Font*> g_FontCache;
 const char* g_FontPath = "romfs:/res/FOT-RodinNTLG Pro DB.otf";
 
 TTF_Font* GetFont(int size)
 {
-    // If the font for this size already exists, return it
     if (g_FontCache.find(size) != g_FontCache.end())
         return g_FontCache[size];
 
-    // Otherwise, load it
     TTF_Font* font = TTF_OpenFont(g_FontPath, size);
-    if (!font) {
+    if (!font)
         return nullptr;
-    }
 
     g_FontCache[size] = font;
     return font;
+}
+
+void FreeFonts()
+{
+    for (auto& f : g_FontCache)
+        TTF_CloseFont(f.second);
+    g_FontCache.clear();
 }
 
 void DrawText(SDL_Renderer *renderer, const char *text, int x, int y, int size, SDL_Color color)
@@ -95,15 +78,113 @@ void DrawText(SDL_Renderer *renderer, const char *text, int x, int y, int size, 
 
     SDL_Rect dstRect = { x, y, textW, textH };
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
-
     SDL_DestroyTexture(texture);
 }
 
-void FreeFonts()
+void DrawChatBuffer(SDL_Renderer *renderer, int startX, int startY, int lineHeight, SDL_Color color)
 {
-    for (auto& f : g_FontCache)
-        TTF_CloseFont(f.second);
-    g_FontCache.clear();
+    int y = startY;
+    for (const auto &line : g_ChatBuffer)
+    {
+        DrawText(renderer, line.c_str(), startX, y, 48, color);
+        y += lineHeight;
+    }
+}
+
+// -----------------------
+// Networking helpers
+// -----------------------
+static bool SetNonBlocking(int sock)
+{
+    if (sock < 0) return false;
+    int flags = fcntl(sock, F_GETFL, 0);
+    if (flags == -1) flags = 0;
+    return fcntl(sock, F_SETFL, flags | O_NONBLOCK) != -1;
+}
+
+int ConnectToServer()
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        return -1;
+
+    SetNonBlocking(sock);
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    int res = connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (res != 0)
+    {
+        if (errno != EINPROGRESS)
+        {
+            AddChatLine("Failed to connect to server.");
+            close(sock);
+            sock = -1;
+        }
+    }
+    return sock;
+}
+
+void send_chat_line(int *sock, const char *username, const char *input)
+{
+    if (!input || input[0] == '\0') return;
+
+    char sendbuf[600];
+    if (username && username[0] != '\0')
+        snprintf(sendbuf, sizeof(sendbuf), "<%s>: %s\n", username, input);
+    else
+        snprintf(sendbuf, sizeof(sendbuf), "%s\n", input);
+
+    if (*sock >= 0) {
+        ssize_t sent = send(*sock, sendbuf, strlen(sendbuf), 0);
+        if (sent < 0) {
+            AddChatLine("Send failed, reconnecting...");
+            close(*sock);
+            *sock = -1;
+        }
+    }
+
+    if (*sock < 0) {
+        *sock = ConnectToServer();
+    }
+}
+
+void TryReceive(int *sock)
+{
+    if (*sock < 0) return;
+
+    char buf[512];
+    while (true) {
+        ssize_t r = recv(*sock, buf, sizeof(buf) - 1, 0);
+        if (r > 0) {
+            buf[r] = '\0';
+            char *start = buf;
+            for (char *p = buf; *p; ++p) {
+                if (*p == '\n') {
+                    *p = '\0';
+                    AddChatLine(start);
+                    start = p + 1;
+                }
+            }
+            if (*start) AddChatLine(start);
+        } else if (r == 0) {
+            AddChatLine("Server disconnected.");
+            close(*sock);
+            *sock = -1;
+            break;
+        } else {
+            if (errno == EWOULDBLOCK || errno == EAGAIN)
+                break;
+            AddChatLine("Socket error, closing.");
+            close(*sock);
+            *sock = -1;
+            break;
+        }
+    }
 }
 
 // -----------------------
@@ -116,42 +197,22 @@ int main(int argc, char **argv)
     romfsInit();
     TTF_Init();
 
-    // Socket setup
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock >= 0) {
-        struct sockaddr_in serverAddr;
-        memset(&serverAddr, 0, sizeof(serverAddr));
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(SERVER_PORT);
-        serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    int sock = ConnectToServer();
 
-        if (connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-            close(sock);
-            sock = -1;
-        }
-    }
-
-    // Variables
     char input[512] = "";
-    int in_len = 0;
-    char recvBuffer[256];
-    
     SDL_Window *window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     SDL_Color black = {0, 0, 0, 255};
-
     std::string scene = "main";
-
     std::string username = "";
-
     std::string textBuffer = "";
+    std::string textSendType = "";
 
     SDL_EventState(SDL_SYSWMEVENT, SDL_ENABLE);
 
-    std::string textSendType = "";
+    AddChatLine("-chat-");
 
-    // Main loop
     while (WHBProcIsRunning()) {
         VPADStatus vpad;
         VPADReadError error;
@@ -159,39 +220,21 @@ int main(int argc, char **argv)
         SDL_WiiUSetSWKBDVPAD(&vpad);
 
         if (error == VPAD_READ_SUCCESS) {
-            // D-pad navigation
-            if (vpad.trigger & VPAD_BUTTON_UP) {
-
-            }
-            if (vpad.trigger & VPAD_BUTTON_DOWN) {
-                
-            }
-            if (vpad.trigger & VPAD_BUTTON_LEFT) {
-                
-            }
-            if (vpad.trigger & VPAD_BUTTON_RIGHT) {
-                
-            }
-
-            // A = Change Username
             if (vpad.trigger & VPAD_BUTTON_A) {
                 textSendType = "username";
                 SDL_WiiUSetSWKBDInitialText(username.c_str());
                 SDL_StartTextInput();
             }
 
-            // B = Send Message
             if (vpad.trigger & VPAD_BUTTON_B) {
                 textSendType = "message";
                 SDL_StartTextInput();
             }
 
-            // L and X = Rules Scene Toggle
-            if ((vpad.trigger & VPAD_BUTTON_L) && scene == "main") {
+            if ((vpad.trigger & VPAD_BUTTON_L) && scene == "main")
                 scene = "rules";
-            } else if ((vpad.trigger & VPAD_BUTTON_X) && scene == "rules") {
+            else if ((vpad.trigger & VPAD_BUTTON_X) && scene == "rules")
                 scene = "main";
-            }
         }
 
         SDL_Event event;
@@ -203,8 +246,8 @@ int main(int argc, char **argv)
                     if (!textBuffer.empty()) {
                         if (textSendType == "message") {
                             strncpy(input, textBuffer.c_str(), sizeof(input) - 1);
-                            in_len = strlen(input);
-                            send_chat_line(&sock, &in_len, input);
+                            input[sizeof(input) - 1] = '\0';
+                            send_chat_line(&sock, username.c_str(), input);
                         } else if (textSendType == "username") {
                             username = textBuffer;
                         }
@@ -216,6 +259,9 @@ int main(int argc, char **argv)
             }
         }
 
+        // Handle incoming messages
+        TryReceive(&sock);
+
         // Render
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
@@ -223,22 +269,20 @@ int main(int argc, char **argv)
         if (scene == "main") {
             DrawText(renderer, "aurorachat", 1300, 10, 96, black);
             DrawText(renderer, "v0.0.2", 1700, 120, 64, black);
-
             DrawText(renderer, "A: Change Username", 0, 20, 64, black);
             DrawText(renderer, "B: Send Message", 0, 110, 64, black);
             DrawText(renderer, "L: Rules", 0, 200, 64, black);
-
             DrawText(renderer, ("Username: " + username).c_str(), 0, 900, 96, black);
+
+            DrawChatBuffer(renderer, 0, 300, 60, black);
         }
         else if (scene == "rules") {
             DrawText(renderer, "(Press X to Go Back)", 0, 20, 64, black);
-
             DrawText(renderer, "Rule 1: No Spamming", 0, 200, 64, black);
             DrawText(renderer, "Rule 2: No Swearing", 0, 380, 64, black);
             DrawText(renderer, "Rule 3: No Impersonating", 0, 560, 64, black);
             DrawText(renderer, "Rule 4: No Politics", 0, 740, 64, black);
-
-            DrawText(renderer, "All of these could result in a ban", 0, 920, 64, black);
+            DrawText(renderer, "Breaking rules may result in a ban", 0, 920, 64, black);
         }
 
         SDL_RenderPresent(renderer);
